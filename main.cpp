c0ed6902863e3c835b3cd4f75966ba955f2d9d64
ut0xxia <90979663+ut0xxia@users.noreply.github.com>
2022-05-30 20:02:49 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,42 +2,19 @@
 #include <cmath>
 #include <../fparser.hh>
 #include <complex>
-#include <math.h>
+#include<vector>
 #define _USE_MATH_DEFINES
 
 const double pi = 4 * atan(1), dt = 0.005;
 FunctionParser_cd fp;
 
-std::complex<double> change(std::complex<double> temp, double a, int place) {
-	double x, y;
-	if (place == 1) {
-		x = temp.real() - a;
-		y = temp.imag() + a;
-	}
-	else if (place == 2) {
-		x = temp.real() + a;
-		y = temp.imag() + a;
-	}
-	else if (place == 3) {
-		x = temp.real() + a;
-		y = temp.imag() - a;
-	}
-	else {
-		x = temp.real() - a;
-		y = temp.imag() - a;
-	}
-	temp = { x, y };
-	return(temp);
-}
-
-int curves_order(std::complex<double> z0, double a)
+int curves_order(double x0, double y0, double a)
 {
-	double x0 = z0.real();
-	double y0 = z0.imag();
-	double angle_temp, sum = 0;
-	std::complex<double>z, f, temp = 0;
+	double angle_temp, sum = 0, p;
+	std::complex<double>z, f, temp;
 	for (double t = dt; t < 2 * pi; t += dt) {
-		z = { x0 + a / 2 * cos(t), y0 + a / 2 * sin(t) };
+		p = std::max(abs(cos(t)), abs(sin(t)));
+		z = { x0 + a * cos(t) / p, y0 + a * sin(t) / p };
 		f = fp.Eval(&z);
 		angle_temp = abs(std::arg(f) - std::arg(temp));
 		if (angle_temp > pi) {
@@ -49,28 +26,60 @@ int curves_order(std::complex<double> z0, double a)
 		sum += angle_temp;
 		temp = f;
 	}
-	return(round(sum / (2 * pi)));
+	return round(sum / (2 * pi));
+}
+std::vector<std::complex<double>>FindRoots(double a, double eps, double x0, double y0, std::vector<std::complex<double>> &roots)
+{
+	double sum = 0;
+	if (curves_order(x0, y0, a) >= 1)
+	{
+		if (a < eps)
+		{
+			bool is_find = false;
+			for (auto el : roots)
+			{
+				if (fabsl(real(el) - x0) <= 2 * eps && fabsl(imag(el) - y0) <= 2 * eps)
+				{
+					is_find = true;
+					break;
+				}
+			}
+			if (!is_find)
+				roots.push_back({(round(x0 / eps) * eps, round(y0 / eps) * eps)});
+		}
+		else
+		{
+			FindRoots(a / 2 + eps / 2, eps, x0 + a / 2, y0 + a / 2, roots);
+			FindRoots(a / 2 + eps / 2, eps, x0 + a / 2, y0 - a / 2, roots);
+			FindRoots(a / 2 + eps / 2, eps, x0 - a / 2, y0 + a / 2, roots);
+			FindRoots(a / 2 + eps / 2, eps, x0 - a / 2, y0 - a / 2, roots);
+		}
+	}
+	return roots;
+}
+
+void find_complex_roots(double a, double x0, double y0, double eps, std::string function)
+{
+	fp.Parse(function, "z");
+	std::complex<double>z = { x0, y0 };
+	std::vector<std::complex<double>> roots;
+	//Вывод
+	for (int i = 0; i < roots.size(); i++) {
+		std::cout << FindRoots(a, eps, x0, y0, roots)[i] << std::endl;
+	}
 }
 int main()
 {
+	setlocale(LC_ALL, "Russian");
 	double a, x0, y0, eps;
 	std::string function;
-	std::complex<double>z;
-
-	std::cout << "a, x0, y0, eps" << std::endl;
-	std::cin >> a >> x0 >> y0 >> eps;
-	std::cout << "f(z)=";
+	std::cout << "Радиус ";
+	std::cin >> a;
+	std::cout <<"Начальная координата ";
+	std::cin >> x0 >> y0;
+	std::cout << "Точность ";
+	std::cin >> eps;
+	std::cout << "Функция F(z) = ";
 	std::cin >> function;
-	fp.Parse(function, "z");
-
-	for (int i = 1; a > 2 * eps; i %= 4) {
-		a /= 2;
-		if (curves_order(change(z, a, i), a) >= 1) {
-			x0 = z.real();
-			y0 = z.imag();
-		}
-	}
-	std::cout << z;
-
 	return 0;
 }
